b59ecf74e22587e564789b5e70c915ba28351d27
sphcastillo <sphcastillo@gmail.com>
2024-07-11 13:26:23 -0700

    CheckoutButton alterations: loading with creating a checkout session in the background ~ added environment variables to Vercel

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -49,10 +49,16 @@ function CheckoutButton() {
   return (
     <div className="flex flex-col space-y-2">
       {/* If subscribed ~ show me the user is subscribed */}
-      <button onClick={() => createCheckoutSession()} className="text-purple-500">Sign Up</button>
-      {/* <div className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default">
-        <button onClick={() => createCheckoutSession()}>Sign Up</button>
-      </div> */}
+
+      <button 
+        onClick={() => createCheckoutSession()}
+        className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white disabled:cursor-default"
+      >
+      {/* Loading while creating a checkout session in the background */}
+        {
+          loading ? 'Loading...' : 'Sign Up Now'
+        }
+      </button>
     </div>
   );
 }
